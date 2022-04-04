518d508820bb654ddf1e2ec905e176a03211db68
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-04 00:22:03 -0400

    id controller test

--- Client/src/test/java/controllers/IdControllerTest.java
+++ Client/src/test/java/controllers/IdControllerTest.java
@@ -3,11 +3,13 @@ package controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import junit.framework.Assert;
 import junit.framework.TestCase;
 import models.Id;
 import models.Message;
 import org.json.simple.JSONArray;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,7 +17,9 @@ public class IdControllerTest extends TestCase {
 
 
     public void testGetIds() throws JsonProcessingException {
-
+        IdController idcont = new IdController();
+        ArrayList<Id> ids = idcont.getIds();
+        Assert.assertTrue(ids.size() != 0);
     }
 
     public void testPostId() {
